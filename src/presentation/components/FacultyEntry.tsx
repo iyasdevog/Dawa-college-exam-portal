b3fae176990c2db792c8279547a8e00691d3357d
iyastk <iyasmvr@gmail.com>
2026-08-11 20:41:55 +0530

    fix: import useCallback in FacultyEntry.tsx

--- src/presentation/components/FacultyEntry.tsx
+++ src/presentation/components/FacultyEntry.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef, useMemo } from 'react';
+import React, { useState, useEffect, useRef, useMemo, useCallback } from 'react';
 import { StudentRecord, SubjectConfig, ClassReleaseSettings } from '../../domain/entities/types';
 import { User } from '../../domain/entities/User';
 import { SYSTEM_CLASSES as CLASSES } from '../../domain/entities/constants';
